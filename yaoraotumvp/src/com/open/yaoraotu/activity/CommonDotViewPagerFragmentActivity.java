bd809d1d8a3ed8739e99df8a10e0fbf5227a52a2
fengmnegchang <624926379@qq.com>
2017-09-15 10:25:03 +0800

    dot pager

---
+++ yaoraotumvp/src/com/open/yaoraotu/activity/CommonDotViewPagerFragmentActivity.java
@@ -0,0 +1,71 @@
+/**
+ *****************************************************************************************************************************************************************************
+ * 
+ * @author :fengguangjing
+ * @createTime:2017-9-15上午10:16:32
+ * @version:4.2.4
+ * @modifyTime:
+ * @modifyAuthor:
+ * @description:
+ *****************************************************************************************************************************************************************************
+ */
+package com.open.yaoraotu.activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+
+import com.open.android.activity.common.CommonCommonFragmentActivity;
+import com.open.yaoraotu.R;
+import com.open.yaoraotu.fragment.CommonPagerFragment;
+import com.open.yaoraotu.utils.UrlUtils;
+
+
+/**
+ *****************************************************************************************************************************************************************************
+ * 
+ * @author :fengguangjing
+ * @createTime:2017-9-15上午10:16:32
+ * @version:4.2.4
+ * @modifyTime:
+ * @modifyAuthor:
+ * @description:
+ *****************************************************************************************************************************************************************************
+ */
+public class CommonDotViewPagerFragmentActivity extends CommonCommonFragmentActivity{
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.open.enrz.activity.CommonFragmentActivity#initValue()
+	 */
+	@Override
+	protected void initValue() {
+		// TODO Auto-generated method stub
+		if (getIntent().getStringExtra("URL") != null) {
+			url = getIntent().getStringExtra("URL");
+		} else {
+			url = UrlUtils.YAORAOTU_TAOTU;
+		}
+		addfragment();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.open.android.activity.CommonFragmentActivity#addfragment()
+	 */
+	@Override
+	public void addfragment() {
+		// TODO Auto-generated method stub
+		super.addfragment();
+		Fragment fragment = CommonPagerFragment.newInstance(url, true);
+		getSupportFragmentManager().beginTransaction().replace(R.id.id_common_fragment, fragment).commit();
+	}
+
+	public static void startCommonDotViewPagerFragmentActivity(Context context, String url) {
+		Intent intent = new Intent();
+		intent.putExtra("URL", url);
+		intent.setClass(context, CommonDotViewPagerFragmentActivity.class);
+		context.startActivity(intent);
+	}
+}
---
+++ yaoraotumvp/src/com/open/yaoraotu/adapter/CommonDotViewPagerAdapter.java
@@ -0,0 +1,76 @@
+/**
+ *****************************************************************************************************************************************************************************
+ * 
+ * @author :fengguangjing
+ * @createTime:2017-9-15上午10:09:06
+ * @version:4.2.4
+ * @modifyTime:
+ * @modifyAuthor:
+ * @description:
+ *****************************************************************************************************************************************************************************
+ */
+package com.open.yaoraotu.adapter;
+
+import java.util.List;
+
+import android.content.Context;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.facebook.drawee.view.SimpleDraweeView;
+import com.open.android.adapter.CommonPagerAdapter;
+import com.open.yaoraotu.R;
+import com.open.yaoraotu.activity.AppWebViewActivity;
+import com.open.yaoraotu.bean.MasonryBean;
+
+/**
+ *****************************************************************************************************************************************************************************
+ * 
+ * @author :fengguangjing
+ * @createTime:2017-9-15上午10:09:06
+ * @version:4.2.4
+ * @modifyTime:
+ * @modifyAuthor:
+ * @description:
+ *****************************************************************************************************************************************************************************
+ */
+public class CommonDotViewPagerAdapter extends CommonPagerAdapter<MasonryBean> {
+
+	public CommonDotViewPagerAdapter(Context mContext, List<MasonryBean> list) {
+		super(mContext, list);
+	}
+	@Override
+	public Object instantiateItem(ViewGroup container, int position) {
+		final MasonryBean bean = (MasonryBean) getItem(position);
+		final ViewHolder mViewHolder = new ViewHolder();
+		View convertView = LayoutInflater.from(mContext).inflate(R.layout.adapter_m_viewpager, null);
+		mViewHolder.draweeview = (SimpleDraweeView) convertView.findViewById(R.id.draweeview);
+		mViewHolder.txt_alt = (TextView) convertView.findViewById(R.id.txt_alt);
+		if (bean != null) {
+			if (bean.getSrc() != null && bean.getSrc().length() > 0) {
+//				DisplayImageOptions options = new DisplayImageOptions.Builder().showStubImage(R.drawable.default_img).showImageForEmptyUri(R.drawable.default_img).showImageOnFail(R.drawable.default_img)
+//						.cacheInMemory().cacheOnDisc().build();
+//				ImageLoader.getInstance().displayImage(bean.getSrc(), mViewHolder.imageview, options, null);
+				mViewHolder.draweeview.setImageURI(Uri.parse(bean.getSrc()));
+			}
+			mViewHolder.txt_alt.setText(bean.getTitle());
+			convertView.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					AppWebViewActivity.startAppWebViewActivity(mContext, bean.getHref());
+				}
+			});
+		}
+		container.addView(convertView);
+		return convertView;
+	}
+
+	private class ViewHolder {
+		SimpleDraweeView draweeview;
+		TextView txt_alt;
+	}
+}
