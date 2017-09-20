cbdbc09e97cad188a79716e63c24f90f87be57f6
Xenon Chau <cto@ido-game.com>
2017-09-20 10:55:50 +0800

    tab mvvm

---
+++ app/src/main/java/com/open/yaoraotu/viewmodel/SubMenuIndicatorViewModel.java
@@ -0,0 +1,22 @@
+package com.open.yaoraotu.viewmodel;
+
+import android.content.Context;
+
+/**
+ * ****************************************************************************************************************************************************************************
+ *
+ * @author :fengguangjing
+ * @createTime: 17/9/20
+ * @version:
+ * @modifyTime:
+ * @modifyAuthor:
+ * @description: ****************************************************************************************************************************************************************************
+ */
+public class SubMenuIndicatorViewModel extends CommonReflectJsoupViewModel{
+
+    public SubMenuIndicatorViewModel(Context mContext, String methodName) {
+        super(mContext, methodName);
+    }
+
+
+}
