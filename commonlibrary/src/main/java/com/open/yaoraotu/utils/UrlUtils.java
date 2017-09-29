765c8966543d2ff00a1e336b0ffa62e9f5f65884
Xenon Chau <cto@ido-game.com>
2017-09-29 13:50:02 +0800

    url 地址更换

--- commonlibrary/src/main/java/com/open/yaoraotu/utils/UrlUtils.java
+++ commonlibrary/src/main/java/com/open/yaoraotu/utils/UrlUtils.java
@@ -27,7 +27,7 @@ public class UrlUtils {
 	public static final String userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3178.0 Safari/537.36";
 	public static final String YAORAOTU = "https://www.yaoraotu.com/";
 	public static final String YAORAOTU_TAOTU="https://www.yaoraotu.com/taotu/";
-	public static final String YAORAOTU_TAOTU_TUIGIRL="https://www.yaoraotu.com/taotu/tuigirl/";
+	public static final String YAORAOTU_TAOTU_TUIGIRL="https://www.yaoraotu.com/meinv/xinggan/";
 	public static final String YAORAOTU_TAOTU_XIURENWANG="https://www.yaoraotu.com/taotu/xiurenwang/";
 	public static final String YAORAOTU_IMAGE="https://www.yaoraotu.com/taotu/xiurenwang/8834.html";
 	public static final String YAORAOTU_M="https://m.yaoraotu.com/";
